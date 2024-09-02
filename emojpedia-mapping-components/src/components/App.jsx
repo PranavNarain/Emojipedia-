043cd0bfc7cedcc71e8a0c26010e2acdc86716ad
PranavNarain <94274853+PranavNarain@users.noreply.github.com>
2024-09-03 01:08:26 +0530

    React jsx, components and mapping.

---
+++ emojpedia-mapping-components/src/components/App.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Entry from "./Entry";
+import emojis from "../emojipedia";
+
+function emojilist(eachemoji) {
+  return (
+    <Entry
+      key={eachemoji.id}
+      name={eachemoji.name}
+      emoji={eachemoji.emoji}
+      meaning={eachemoji.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojis.map(emojilist)}</dl>
+      {/* 
+
+      
+      <dl className="dictionary">
+        <Entry />
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🙏
+            </span>
+            <span>Person With Folded Hands</span>
+          </dt>
+          <dd>
+            Two hands pressed together. Is currently very introverted, saying a
+            prayer, or hoping for enlightenment. Is also used as a “high five”
+            or to say thank you.
+          </dd>
+        </div>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🤣
+            </span>
+            <span>Rolling On The Floor, Laughing</span>
+          </dt>
+          <dd>
+            This is funny! A smiley face, rolling on the floor, laughing. The
+            face is laughing boundlessly. The emoji version of “rofl“. Stands
+            for „rolling on the floor, laughing“.
+          </dd>
+        </div>
+      </dl> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ emojpedia-mapping-components/src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "❤️",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.",
+  },
+  {
+    id: 5,
+    emoji: "😎",
+    name: "Smiling Face with Sunglasses",
+    meaning:
+      "A yellow face with a broad, closed smile wearing black sunglasses, as if a pair of classic Wayfarers. Often used to convey the slang sense of cool. May also express a confident, carefree attitude or that something is excellent.",
+  },
+  {
+    id: 6,
+    emoji: "🤗",
+    name: "Smiling Face with Open Hands",
+    meaning:
+      "A yellow face smiling with open hands, as if giving a hug. May be used to offer thanks and support, show love and care, or express warm, positive feelings more generally.",
+  },
+  {
+    id: 7,
+    emoji: "🤡",
+    name: "Clown Face",
+    meaning:
+      "A circus or birthday clown displayed with white face makeup, a red nose, exaggerated eyes and smile, and two tufts of hair (red or blue). Commonly used as comment on TikTok or other social media platforms to indicate that a person is foolish, idiotic and/or selfish.",
+  },
+  {
+    id: 8,
+    emoji: "😁",
+    name: "Beaming Face with Smiling Eyes",
+    meaning:
+      "A yellow face with smiling eyes and full-toothed grin, as if saying Cheese! for the camera. Teeth may be smoothed-over or crosshatched. Often expresses a radiant, gratified happiness. Tone varies, including warm, silly, amused, or proud.",
+  },
+  {
+    id: 9,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "A yellow face with an open smile, sometimes showing teeth, and red, cartoon-styled hearts for eyes. Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing.",
+  },
+];
+
+export default emojipedia;
